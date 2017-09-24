e19dfa12025877f76cbb90624554639e95ecb93b
Alexander CHernyaev <doc546@rambler.ru>
2017-09-24 21:17:59 +0300

    Bound callable references

--- src/com/github/phantasmdragon/Main.kt
+++ src/com/github/phantasmdragon/Main.kt
@@ -2,4 +2,8 @@ package com.github.phantasmdragon
 
 fun main(args: Array<String>) {
 
+    val numRegex = "\\d+".toRegex()
+    val numbers = listOf("led", "42", "666", "4fun").filter(numRegex::matches)
+
+    print("Result is $numbers")
 }
